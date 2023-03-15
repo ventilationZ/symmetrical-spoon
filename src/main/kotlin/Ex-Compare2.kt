45c65b684247ded2d33f223c02f8ea003000bd57
eMobilis <elikibetael4@gmail.com>
2023-03-15 10:26:13 +0300

    simple test examples

---
+++ src/main/kotlin/Ex-Compare2.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    print("Enter Your Mean Marks")
+    var mrks:Int = readLine()!!.toInt()
+    if(mrks >=80){
+        print("You Got Grade A ")
+    }else if(mrks >=70 ){
+        print("You Got Grade B ")
+    }
+    else if(mrks >=60 ){
+        print("You Got Grade C ")
+    }
+    else if(mrks >=50 ){
+        print("You Got Grade D ")
+    }
+    else if(mrks >=0 ){
+        print("You Got Grade E You have to repeat the exam")
+    }
+
+
+}
