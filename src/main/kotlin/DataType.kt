bbbbc933e34a338a05b3d512436eceb8f4e6c769
eMobilis <elikibetael4@gmail.com>
2023-03-15 08:53:33 +0300

    data types in kotlin

---
+++ src/main/kotlin/DataType.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+    //Integers
+    var age = 20
+    var age:Int = 32
+
+    //Strings
+    var name:String = "Dj Bash"
+    var name = "Dj Greg"
+
+    //Chars
+    var city = 'f'
+
+    //Booleans
+    var status = true
+    var status = false
+
+    //Double
+    var temp = 36.51
+
+    //Float
+    var lat:Float =65.5F
+
+
+
+}
