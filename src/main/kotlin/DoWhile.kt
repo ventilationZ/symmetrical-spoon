2dbb0aee4f15f24084f230e6ae98cbe5aac1ab2f
eMobilis <elikibetael4@gmail.com>
2023-03-21 09:23:47 +0300

    simple test examples .....

---
+++ src/main/kotlin/DoWhile.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+
+    var numbe = 0
+    do {
+        numbe++
+        println(numbe)
+    }
+        while (numbe <= 10)
+}
---
+++ src/main/kotlin/WhileLoop.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var counter =  0
+    while(counter <= 31)
+    {
+        println(counter)
+        counter++
+    }
+}
