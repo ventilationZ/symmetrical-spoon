e63b2138951bbbe4fefe425ed43a0bc907c235ce
eMobilis <elikibetael4@gmail.com>
2023-03-23 08:18:02 +0300

    simdata types2 .......

--- src/main/kotlin/DataType.kt
+++ src/main/kotlin/DataType.kt
@@ -1,5 +1,16 @@
-fun main(args: Array<String>) {
-   //
-
-
-}
+//fun main(args: Array<String>) {
+//    //Integer or Int
+//    var age:Int = 20
+//
+//    //Strings
+//    var name:String = "Muraya"
+//    //Double
+//    var temperature:Double = 36.51
+//    //Boolean true or false
+//    var Status = false
+//    //Float
+//    var point:Float = 8.5F
+//    //Convert double to float
+//    var marks = 46.587F
+//
+//}
